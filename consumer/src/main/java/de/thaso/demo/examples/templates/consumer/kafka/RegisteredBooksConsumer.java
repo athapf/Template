64129c25259864d9b34742071f509a4fa3148b90
athaler <athaler001@thaso.de>
2022-11-14 08:51:59 +0100

    revert to start point

--- consumer/src/main/java/de/thaso/demo/examples/templates/consumer/kafka/RegisteredBooksConsumer.java
+++ consumer/src/main/java/de/thaso/demo/examples/templates/consumer/kafka/RegisteredBooksConsumer.java
@@ -3,14 +3,10 @@ package de.thaso.demo.examples.templates.consumer.kafka;
 import de.thaso.demo.examples.templates.consumer.data.ManagementDao;
 import de.thaso.demo.examples.templates.consumer.kafka.dto.BookDto;
 import de.thaso.demo.examples.templates.consumer.kafka.utils.BookMapper;
-import de.thaso.demo.examples.templates.consumer.service.LibraryService;
 import de.thaso.demo.examples.templates.consumer.service.MyBook;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.kafka.KafkaRecord;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.OnOverflow;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -24,21 +20,14 @@ public class RegisteredBooksConsumer {
     @Inject
     private BookMapper bookMapper;
 
-    @Channel("work")
-    @OnOverflow(OnOverflow.Strategy.DROP)
-    private Emitter<String> stringEmitter;
-
     @Inject
     private ManagementDao managementDao;
 
     @Blocking
     @Incoming("registeredBooks")
     public CompletionStage<Void> consumeBook(final KafkaRecord<Long, BookDto> kafkaRecord) {
-
         final MyBook book = bookMapper.bookDtoToMyBook(kafkaRecord.getPayload());
         managementDao.insertEmployee(book);
-        stringEmitter.send(book.getId().toString());
-
         return kafkaRecord.ack();
     }
 }
--- consumer/src/main/java/de/thaso/demo/examples/templates/consumer/rest/LibraryResource.java
+++ consumer/src/main/java/de/thaso/demo/examples/templates/consumer/rest/LibraryResource.java
@@ -1,12 +1,6 @@
 package de.thaso.demo.examples.templates.consumer.rest;
 
-import de.thaso.demo.examples.templates.consumer.rest.dto.StockDto;
-import de.thaso.demo.examples.templates.consumer.rest.utils.BookMapper;
-import de.thaso.demo.examples.templates.consumer.rest.utils.StockMapper;
 import de.thaso.demo.examples.templates.consumer.service.LibraryService;
-import de.thaso.demo.examples.templates.consumer.service.MyStock;
-import io.smallrye.mutiny.Multi;
-import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -16,7 +10,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.Random;
 
 @Path("/library")
 @Produces(MediaType.APPLICATION_JSON)
@@ -29,34 +22,11 @@ private static final Logger LOGGER = Logger.getLogger(LibraryResource.class);
     @Inject
     private LibraryService libraryService;
 
-    @Inject
-    private StockMapper stockMapper;
-
     @GET
     @Path("reset")
     public void doReset() {
         LOGGER.info("doReset ...");
 
         libraryService.doReset();
-    }
-
-    @Channel("work")
-    private Multi<String> stringConsumer;
-
-    private Random random = new Random(System.currentTimeMillis());
-
-    @GET
-    @Path("{pos}/stock")
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    public Multi<StockDto> stockInfo(final String pos) {
-        LOGGER.info("stockInfo ...");
-
-        return stringConsumer.log().select().where(s -> s.equals(pos))
-            .select().first()
-            .onItem().transform(id -> MyStock.builder()
-                .withPos(id)
-                .withCount(random.nextInt(900) + 100)
-                .build()).onItem().transform(
-                item -> stockMapper.myStockToStockDto(item));
     }
 }
